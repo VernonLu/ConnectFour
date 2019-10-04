fc2250ee99b2b9ac7eb41346a4a549777a1ca70e
VernonLu <55864722+VernonLu@users.noreply.github.com>
2019-10-04 10:17:06 -0700

    Vernon

--- ConnectFour/ConnectFour.cpp
+++ ConnectFour/ConnectFour.cpp
@@ -1,39 +1,64 @@
 #include <iostream>
 using namespace std;
 
-char** map;
-int col, row;
+
+//char** map;
+const int col = 7, row = 6;
+char map[row][col];
+const char player[2] = { 'O','X' };
+int curtPlayer = 0;
+
+
 void init() {
-	map = new char* [row];
 	for (int i = 0; i < row; i++) {
-		data[i] = new char[col];
+		for (int j = 0; j < col; j++) {
+			map[i][j] = '-';
+		}
 	}
-
-	/*for (auto c : map) {
-		c = '-';
-	}*/
-
 }
 
 
 void showMap() {
-	/*for (int i = 0; i < y; i++) {
-		for (int j = 0; j < x; j++) {
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
 			cout << map[i][j] << " ";
 		}
 		cout << endl;
-	}*/
+	}
 }
+
+void showPlayer() {
+	cout << "Player " << player[curtPlayer] << "'s turn, Please Enter 1-7: ";
+}
+
+bool check() {
+	return true;
+}
+
 void gameLoop() {
-	gameLoop();
+	showMap();
+	showPlayer();
+	//char in = cin.get();
+	char in = 0;
+	cin.get(in);
+
+	if ((int)in > 48 && (int)in < 56) {
+		in -= 49;
+		curtPlayer = (curtPlayer + 1) % 2;
+	}
+	cout << "you entered " << (int)in << endl;
 }
 
 
 int main()
 {
-    cout << "Welcome to Connect Four!\n";
+
+
 	init();
-	showMap();
-	//gameLoop();
+	while (check()) {
+		gameLoop();
+	}
+
+
 	return 0;
 }
