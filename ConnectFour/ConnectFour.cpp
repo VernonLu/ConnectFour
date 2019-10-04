78578d4fe6cdcc41e0d244d4b0cf924d208ebe67
VernonLu <lht13615200707@gmail.com>
2019-10-04 08:43:16 -0700

    Vernon

--- ConnectFour/ConnectFour.cpp
+++ ConnectFour/ConnectFour.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 using namespace std;
 
-char* map;
-
+char** map;
+int col, row;
 void init() {
+	map = new char* [row];
+	for (int i = 0; i < row; i++) {
+		data[i] = new char[col];
+	}
 
+	/*for (auto c : map) {
+		c = '-';
+	}*/
 
 }
 
+
+void showMap() {
+	/*for (int i = 0; i < y; i++) {
+		for (int j = 0; j < x; j++) {
+			cout << map[i][j] << " ";
+		}
+		cout << endl;
+	}*/
+}
 void gameLoop() {
-	int x = 0;
-	x = cin.get();
 	gameLoop();
 }
 
@@ -19,7 +33,7 @@ int main()
 {
     cout << "Welcome to Connect Four!\n";
 	init();
-
-	gameLoop();
-
+	showMap();
+	//gameLoop();
+	return 0;
 }
