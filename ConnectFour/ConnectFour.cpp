292cb9f996ed1c460368180439eb246f5477ea9f
VernonLu <55864722+VernonLu@users.noreply.github.com>
2019-10-03 17:10:48 -0700

    Create Project

---
+++ ConnectFour/ConnectFour.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+char* map;
+
+void init() {
+
+
+}
+
+void gameLoop() {
+	int x = 0;
+	x = cin.get();
+	gameLoop();
+}
+
+
+int main()
+{
+    cout << "Welcome to Connect Four!\n";
+	init();
+
+	gameLoop();
+
+}
